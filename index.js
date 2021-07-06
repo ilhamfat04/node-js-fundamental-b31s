80c24e7cc547a9469dc358daf63db03534a498a9
Jody Septiawan <jody@192.168.1.2>
2021-07-06 14:13:19 +0700

    Hello World with ExpressJs

---
+++ index.js
@@ -0,0 +1,9 @@
+//instantiate express module here
+
+//use express in app variable here
+
+//define the server port here
+
+//create the homepage route here and inside it create res means, response, and it send string "Hello Express!" to the API
+
+// Create listen here
