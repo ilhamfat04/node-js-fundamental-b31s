7d6d2a8b269b96c68e051dc198b472e9e3dc2d98
= <=>
2022-03-08 13:46:34 +0700

    update

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = 5000
 //create the homepage route
 app.get('/', (req, res) => {
     //res means, response, and it send string "Hello Express!" to the API
-    res.send('Hello Express!')
-})    
+    res.send('Hello Express batch 31!')
+})
 
 //when this nodejs app executed, it will listen to defined port
 app.listen(port, () => console.log(`Listening on port ${port}!`))
