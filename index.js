1c1c222252b96594e1616e8e72ec8958193d2e6a
= <=>
2022-03-08 13:35:37 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 //instantiate express module here
+const express = require('express')
 
 //use express in app variable here
+const app = express()
 
 //define the server port here
+const port = 5000
 
 //create the homepage route here and inside it create res means, response, and it send string "Hello Express!" to the API
+app.get('/', (req, res) => {
+    res.send('Hello express BATCH 31')
+})
 
 // Create listen here
+app.listen(port, () => console.log(`Listening on port ${port}`))
